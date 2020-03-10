39480311e4a373b34d6e1b5263f8893d0921d04e
Peter Simmons <peterorpete@me.com>
2020-03-10 23:33:01 +0000

    added images and some css changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,15 +6,15 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Learn to <br />design and code React apps</h1>
-    <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+  <div>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Learn to <br />design and code React apps</h1>
+        <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
+        <Link to="/page-2/">Watch the video</Link>
+      </div>
     </div>
-    <Link to="/page-2/">Watch the video</Link>
-  </Layout>
+  </div>
 )
 
 export default IndexPage
